146c37b7261fb23ac0af8dc8b0848ed93c0675fb
hs <tlsgustn93@naver.com>
2024-02-26 19:10:15 +0900

    fix/push에러 수정
    This reverts commit c12bb1fbc5603ae655d1d002105b11c70ecc72f4.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import Link from "next/link";
-
 export default function Home() {
-  return (
-    <main>
-      main
-      <Link href={"/login"}>login</Link>
-    </main>
-  );
+  return <main>main</main>;
 }
