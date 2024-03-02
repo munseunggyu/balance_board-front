969ecf91d9515591abd3b79ff32ec3b50307cb93
hs <tlsgustn93@naver.com>
2024-03-02 23:06:47 +0900

    feat/writing 로그인만 접근

--- src/app/writing/page.tsx
+++ src/app/writing/page.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from "react";
 
+import { useUserDataContext } from "@/context/AuthContext";
+
 import WritingCategory from "./_component/WritingCategory";
 import WritingContent from "./_component/WritingContent";
 import WritingForm from "./_component/WritingForm";
@@ -12,6 +14,7 @@ import WritingVoteInput from "./_component/WritingVoteInput";
 import styles from "./writing.module.css";
 
 export default function Writing() {
+  const { userInfo } = useUserDataContext();
   const [selectedCategory, setSelecteCategory] = useState<string | null>("");
   const [title, setTitle] = useState<string | null>("");
   const [content, setContent] = useState<string | null>("");
@@ -39,32 +42,37 @@ export default function Writing() {
   const handleTags = (newTags: string[]) => {
     setTags(newTags);
   };
+  console.log(userInfo);
 
   return (
     <div className={styles.writingBox}>
-      <WritingNav title={"Writing"}>
-        <WritingForm
-          selectedCategory={selectedCategory}
-          title={title}
-          content={content}
-          option1={option1}
-          option2={option2}
-          tags={tags}
-        />
-      </WritingNav>
-      <div className={styles.writingContainer}>
-        <WritingCategory selectedOption={selectedCategory} OnSelectedOption={handleCategorySelect} />
-        <WritingTitle onTitleData={handleTitle} />
-        <WritingContent onContentData={handleContent} />
-      </div>
-      <div className={styles.tagContainer}>
-        <WritingTag onTagsData={handleTags} />
-        <div className={styles.seperator}></div>
-      </div>
-      <div className={styles.writingVoteContainer}>
-        <div className={styles.voteItem}>투표 항목</div>
-        <WritingVoteInput onVoteData={handleVoteOption} />
-      </div>
+      {userInfo.isLogin === 1 && (
+        <>
+          <WritingNav title={"Writing"}>
+            <WritingForm
+              selectedCategory={selectedCategory}
+              title={title}
+              content={content}
+              option1={option1}
+              option2={option2}
+              tags={tags}
+            />
+          </WritingNav>
+          <div className={styles.writingContainer}>
+            <WritingCategory selectedOption={selectedCategory} OnSelectedOption={handleCategorySelect} />
+            <WritingTitle onTitleData={handleTitle} />
+            <WritingContent onContentData={handleContent} />
+          </div>
+          <div className={styles.tagContainer}>
+            <WritingTag onTagsData={handleTags} />
+            <div className={styles.seperator}></div>
+          </div>
+          <div className={styles.writingVoteContainer}>
+            <div className={styles.voteItem}>투표 항목</div>
+            <WritingVoteInput onVoteData={handleVoteOption} />
+          </div>
+        </>
+      )}
     </div>
   );
 }
