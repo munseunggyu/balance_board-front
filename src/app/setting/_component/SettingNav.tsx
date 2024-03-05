35c8db15fe7db869185245432c60d0f5d7ab995c
hs <tlsgustn93@naver.com>
2024-03-05 22:00:25 +0900

    fix/이미지 경로 수정

--- src/app/setting/_component/SettingNav.tsx
+++ src/app/setting/_component/SettingNav.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import backPageImg from "../../../../public/direction-left-md.svg";
 import styles from "./settingNav.module.css";
 
 export default function SettingNav({ title }: { title?: string }) {
@@ -11,7 +12,7 @@ export default function SettingNav({ title }: { title?: string }) {
       <div className={styles.navbarContainer}>
         <div className={styles.backPage}>
           <Link href="/">
-            <Image src="/image5.png" alt="Page 1" width={24} height={24} />
+            <Image src={backPageImg} alt="뒤로 가기 버튼" width={24} height={24} />
           </Link>
         </div>
         <div className={styles.title}>{title}</div>
