37eb13064248419c62b535cb237d5fd890011af2
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-03-09 00:08:30 +0900

    Feat: sitemap, robots url 재 설정

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
         allow: "/",
       },
     ],
-    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
+    sitemap: `https://balanceboard.swygbro.com/sitemap.xml`,
   };
 }
