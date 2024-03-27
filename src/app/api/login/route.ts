d3f315ef3c5f0033fde4b15020ef603e4ad8d4d0
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-03-27 18:33:16 +0900

    Feat: 로그인 기능 Next api에 추가 cookie 설정

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,39 @@
+import dayjs from "dayjs";
+import { jwtDecode } from "jwt-decode";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+import { ILogin } from "@/app/login/_component/LoginForm";
+import { constant } from "@/utils/constant";
+
+export async function POST(req: NextRequest) {
+  const { email, password } = await req.json();
+  const res = await fetch(constant.apiUrl + "api/user/login", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
+  const data: ILogin = await res.json();
+  if (data.message) {
+    return NextResponse.json({ data });
+  }
+  const token = data.jwtToken.accessToken;
+  const refreshToken = data.jwtToken.refreshToken;
+  const tokenDecode = jwtDecode(token);
+  const refreshTokenDecode = jwtDecode(refreshToken);
+  if (!tokenDecode.exp) return NextResponse.json({ data });
+  if (!refreshTokenDecode.exp) return NextResponse.json({ data });
+
+  const refreshTokenTime = dayjs.unix(refreshTokenDecode.exp).toDate();
+
+  cookies().set("token", token);
+  cookies().set("refreshToken", refreshToken, { expires: refreshTokenTime });
+
+  return NextResponse.json(data);
+}
