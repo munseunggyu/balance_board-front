f64de3e06f26731e496d02fce9c411f75c2428d1
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-03-27 18:33:43 +0900

    Feat: 토큰 기능 Next api에 추가 cookie 설정

---
+++ src/app/api/refresh/token/route.ts
@@ -0,0 +1,41 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+import { IToken } from "@/app/_component/TokenLoginComponent";
+import { constant } from "@/utils/constant";
+
+export async function GET() {
+  const cookieStore = cookies();
+  let cookieAccessToken = "";
+  let cookieRefreshToken = "";
+  cookieStore.getAll().forEach((v) => {
+    if (v.name === "token") {
+      cookieAccessToken = v.value;
+    }
+    if (v.name === "refreshToken") {
+      cookieRefreshToken = v.value;
+    }
+  });
+  const res = await fetch(constant.apiUrl + "api/user/login/token", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${cookieAccessToken}`,
+      refreshToken: cookieRefreshToken,
+    },
+    credentials: "include",
+  });
+  const data: IToken = await res.json();
+  if (data.message) {
+    cookies().delete("token");
+    cookies().delete("refreshToken");
+    return NextResponse.json({ data });
+  }
+  const token = data.accessToken;
+  if (cookieStore.has("token")) {
+    cookies().delete("token");
+  }
+  cookies().set("token", token);
+
+  return NextResponse.json(data);
+}
