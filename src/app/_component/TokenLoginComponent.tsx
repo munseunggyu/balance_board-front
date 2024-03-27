3e3685dd9a09b82cdca0a786cbcf5ecba5b7ea7e
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-03-27 18:33:36 +0900

    Feat: 토큰 로그인 기능 Next api에 추가 cookie 설정

--- src/app/_component/TokenLoginComponent.tsx
+++ src/app/_component/TokenLoginComponent.tsx
@@ -1,53 +1,53 @@
 "use client";
+import dayjs from "dayjs";
+import { jwtDecode } from "jwt-decode";
 import React, { useEffect } from "react";
 
 import { useUserDataContext } from "@/context/AuthContext";
 import { constant } from "@/utils/constant";
+// import { cookies } from "next/headers";
 
-interface IToken {
+export interface IToken {
   email: string;
   accessToken: string;
   userId: number;
   imageType: number;
   nickname: string;
+  message: string;
+}
+interface IProps {
+  accessToken: string | null;
+  refreshToken: string | null;
 }
 
-export default function TokenLoginComponent() {
-  const { setUserData } = useUserDataContext();
-  let token: string | null = null;
-  let refreshToken: string | null = null;
-  if (typeof window !== "undefined") {
-    // Client-side-only code
-    token = localStorage.getItem("token");
-    refreshToken = localStorage.getItem("refreshToken");
-  }
+export default function TokenLoginComponent({ accessToken, refreshToken }: IProps) {
+  const { setUserData, logoutContext } = useUserDataContext();
 
   const tokenLogin = async () => {
-    console.log(token, refreshToken);
-    if (!token || !refreshToken) {
-      setUserData({
-        email: "",
-        jwtToken: {
-          accessToken: "",
-          refreshToken: "",
-        },
-        nickname: "",
-        userId: 0,
-        isLogin: 2,
-        imageType: 0,
+    if (!accessToken || !refreshToken) {
+      await fetch(constant.baseUrl + "api/logout", {
+        method: "GET",
       });
+      logoutContext();
       return;
     }
-    const res = await fetch(constant.apiUrl + "api/user/login/token", {
+    const res = await fetch(constant.baseUrl + "api/refresh/token", {
       method: "GET",
       headers: {
         Accept: "application/json",
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${accessToken}`,
         refreshToken: refreshToken,
       },
       credentials: "include",
     });
     const data: IToken = await res.json();
+    if (data.message) {
+      await fetch(constant.baseUrl + "api/logout", {
+        method: "GET",
+      });
+      logoutContext();
+      return;
+    }
     const userData = {
       ...data,
       jwtToken: {
@@ -67,8 +67,60 @@ export default function TokenLoginComponent() {
       });
     }
   };
+
+  /**
+   *
+   * @param type 1. token 2. refreshToken
+   */
+  const checktokenValidation = (type: number, token: string) => {
+    if (!token) return false;
+    const res = jwtDecode(token);
+    const exp = res.exp;
+    if (!exp) return false;
+    let tokenTime;
+    if (type === 1) {
+      tokenTime = dayjs.unix(exp).subtract(10, "minute");
+    } else {
+      tokenTime = dayjs.unix(exp);
+    }
+    const isBefore = tokenTime.isBefore(dayjs());
+    return isBefore;
+  };
+  const tokenCheck = async () => {
+    if (!accessToken || !refreshToken) {
+      await fetch(constant.baseUrl + "api/logout", {
+        method: "GET",
+      });
+      logoutContext();
+      return;
+    }
+    // const tokenValidation = checktokenValidation(1, accessToken);
+    // if (!firstLoad) {
+    //   if (!tokenValidation) return;
+    // }
+    // setFirstLoad(false);
+
+    const refreshTokenValidation = checktokenValidation(2, refreshToken);
+    if (refreshTokenValidation) {
+      // 로그아웃
+      void fetch(constant.baseUrl + "api/logout", {
+        method: "GET",
+      });
+      logoutContext();
+    } else {
+      void tokenLogin();
+    }
+  };
   useEffect(() => {
-    void tokenLogin();
+    void tokenCheck();
+    const interval = setInterval(
+      () => {
+        void tokenCheck();
+      },
+      5 * 60 * 1000,
+    ); // 5분에 한 번씩 실행되도록 설정
+
+    return () => clearInterval(interval);
   }, []);
   return <></>;
 }
