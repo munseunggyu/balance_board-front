4fe3cf0fb5f0f691ff444a999f62730d1da78e79
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-04-11 22:15:17 +0900

    Feat: 사이트 리뉴얼로 인해 리얼 서버 다운으로 인한 버그 사이트맵 주석처리 추후 다시 사용 robots.ts 주석

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
         allow: "/",
       },
     ],
-    sitemap: `https://balanceboard.swygbro.com/sitemap.xml`,
+    // sitemap: `https://balanceboard.swygbro.com/sitemap.xml`,
   };
 }
