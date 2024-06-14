ce88740a4f4fe3fa96466d6e01dfa03220e1fc09
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-06-14 22:11:35 +0900

    Feat: 메인 화면 중간 커밋을 위한 텍스트로 대체

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 import MainLayout from "./_main/page";
 
-export default function Home({ children }: { children: React.ReactNode }) {
-  return <MainLayout isHome={false}>{children}</MainLayout>;
+export default function Home() {
+  return <MainLayout isHome={false}>메인 화면</MainLayout>;
 }
